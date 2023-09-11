998bb19894fd452d67bd60f84912a582fa6c6dc8
Ksenia Pilshchikova <ksushapi@gmail.com>
2023-09-11 21:21:24 +0200

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 
-const { calcTriangulation, calcSmoothTriangulation } = require('./calc-triangulation');
+const calcTriangulation = require('./calc-triangulation');
 
 const app = express();
 
@@ -27,18 +27,6 @@ app.post('/triangulate-cone', (req, res) => {
     }
     
     const triangles = calcTriangulation(height, radius, segments);
-
-    return res.json({ triangles });
-});
-
-app.post('/smooth-cone', (req, res) => {
-    const { height, radius, segments } = req.body;
-
-    if (radius <= 0 || segments < 3) {
-        return res.status(400).json({ error: 'The radius must be greater than 0, and the number of segments must be at least 3.' });
-    }
-    
-    const triangles = calcSmoothTriangulation(height, radius, segments);
 
     return res.json({ triangles });
 });
